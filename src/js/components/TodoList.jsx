963638138a57345d309b2fb4e32baf2eaaacc7d0
Edgar Cuenca <edgar.3ch@gmail.com>
2026-02-21 04:44:48 +0000

    se eliminan las tareas y se agrega mensaje de invitacion

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -2,20 +2,19 @@ import React, { useState } from "react";
 
 const TodoList = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState(["Make the bed", "Wash my hands"]);
+  
 
-  // Agregar tarea al presionar Enter
+  const [todos, setTodos] = useState([]);
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && inputValue.trim() !== "") {
       setTodos([...todos, inputValue]);
-      setInputValue(""); // Limpiar el input
+      setInputValue("");
     }
   };
 
-  // Eliminar tarea por índice
   const deleteTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
-    setTodos(newTodos);
+    setTodos(todos.filter((_, i) => i !== index));
   };
 
   return (
@@ -32,14 +31,22 @@ const TodoList = () => {
               onKeyDown={handleKeyDown}
             />
           </li>
-          {todos.map((task, index) => (
-            <li key={index} className="task-item">
-              {task}
-              <span className="delete-icon" onClick={() => deleteTodo(index)}>
-                ✕
-              </span>
+
+
+          {todos.length === 0 ? (
+            <li className="task-item text-muted justify-content-center">
+              No tasks, add a task
             </li>
-          ))}
+          ) : (
+            todos.map((task, index) => (
+              <li key={index} className="task-item">
+                {task}
+                <span className="delete-icon" onClick={() => deleteTodo(index)}>
+                  ✕
+                </span>
+              </li>
+            ))
+          )}
         </ul>
         <div className="footer">{todos.length} items left</div>
       </div>
